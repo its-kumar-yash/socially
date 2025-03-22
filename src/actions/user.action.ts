1984a29aeb3450adcd1441dd3d5cc2c699078e65
its-kumar-yash <its.yash.kumar23@gmail.com>
2025-03-22 21:34:01 +0530

    added db sync (clerk + neondb)

---
+++ src/actions/user.action.ts
@@ -0,0 +1,32 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser(){
+    try{
+        const {userId} = await auth();
+        const user = await currentUser();
+        if(!userId || !user) return;
+        //check if user already exists
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                clerkId: userId
+            }
+        })
+        if(existingUser) return existingUser;
+        //create user
+        const dbUser = await prisma.user.create({
+            data: {
+                clerkId: userId,
+                name: `${user.firstName || ""} ${user.lastName || ""}`,
+                username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+                email: user.emailAddresses[0].emailAddress,
+                image: user.imageUrl
+            }
+        })
+
+    } catch(e){
+        console.error(e);
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <h1>Home Page</h1>
